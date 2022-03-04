0f8093d13587e0be7df3acf9cecde0a88f6f401d
lukmanx <archer.lukman@gmail.com>
2022-03-04 07:13:12 +0700

    get to know object type and how to define its type static

--- basic_typescript/index.js
+++ basic_typescript/index.js
@@ -2,3 +2,25 @@ function addition(n1, n2) {
     return n1 > n2;
 }
 console.log(addition(12, 12));
+// TYPE ASSIGNMENT AND TYPE INFERENCE
+var x = 1;
+var y = 2;
+var z = 'testing';
+// UNDERSTANDING OBJECT IN Typescript
+// --> define person as an 'object' or {key: string, key: nujber}!
+var person = {
+    name: 'ben',
+    age: 10
+};
+console.log(person.name);
+console.log(person);
+var product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+};
+console.log(product);
--- basic_typescript/index.ts
+++ basic_typescript/index.ts
@@ -1,6 +1,46 @@
-function addition(n1: number, n2: number):boolean {
+function addition(n1: number, n2: number): boolean {
     return n1 > n2
 }
 
 
 console.log(addition(12, 12))
+
+
+// TYPE ASSIGNMENT AND TYPE INFERENCE
+const x: number = 1;
+const y: number = 2;
+const z: string = 'testing';
+
+// UNDERSTANDING OBJECT IN Typescript
+// --> define person as an 'object' or {key: string, key: nujber}!
+const person: {
+    name: string,
+    age: number
+} = {
+    name: 'ben',
+    age: 10,
+}
+console.log(person.name)
+console.log(person)
+
+
+const product: {
+    id: string;
+    price: number;
+    tags: string[];
+    details: {
+        title: string;
+        description: string;
+    }
+} = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+}
+
+console.log(product)
+
